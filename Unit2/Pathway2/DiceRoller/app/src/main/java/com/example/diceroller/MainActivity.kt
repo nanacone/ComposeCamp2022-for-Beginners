905e939e96889ef4d1ba527282aa8feccc6b6881
nyahn <nyahn@markany.com>
2022-12-23 15:56:12 +0900

    2022 12 23 by nanacone - 주석 추가

--- Unit2/Pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit2/Pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -42,7 +42,12 @@ fun DiceRollerApp() {
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     // 버튼 클릭 및 이미지 표시 로직 보유
+    /**
+     * result = 1 : 이런식이면 result 값을 보유하고 있지 않기 때문에 계속 1로 재설정 됨.
+     * -> remember 컴포저블을 사용하여 메모리에 객체를 저장할 수 있음
+     */
     var result by remember { mutableStateOf(1)}
+
     val imageResource = when(result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -53,7 +58,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     }
     Column (
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally // 가운데에 정렬하도록 도와줌
     ) {
         Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
         Spacer(modifier = Modifier.height(16.dp))
